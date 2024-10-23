d8ac3affbf54840b11bb1ade1d814b40b3583a79
Alan <Alan@DESKTOP-0O2VDL4>
2024-10-23 20:58:45 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './Navbar';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
   return (
--- src/Navbar.js
+++ src/Navbar.js
@@ -5,18 +5,29 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <h2>My E-commerce</h2>
+        <h2>Compro todo</h2>
       </div>
-      <form>
-        <input className='buscar' placeholder='Buscar productos' ></input>
-     </form>
+      <form className="d-flex">
+          <input className='form-control me-2 buscar' type="search" placeholder='Buscar productos' aria-label='Search' />
+          <button className='btn btn-outline' type='submit'>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              className='bi bi-search'
+              viewBox="0 0 16 16"
+            >
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+            </svg>
+          </button>
+        </form>
       <ul className="navbar-links">
         <li><a href="/">Home</a></li>
         <li><a href="/products">Products</a></li>
         <li><a href="/about">About Us</a></li>
         <li><a href="/cart">Cart</a></li>
       </ul>
-   
     </nav>
   );
 };
